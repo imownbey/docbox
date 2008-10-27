ee41858f4e3f0e7f41999c3aa7e940002a15f9d6
Ian Ownbey <ian@inspir.es>
2008-10-27 15:25:05 -0700

    Added 'add' 'delete' and 'change'

--- spec/models/change_spec.rb
+++ spec/models/change_spec.rb
@@ -3,6 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Change do
   before(:each) do
     @valid_attributes = {
+      :type => "value for type",
+      :tag_id => "1",
+      :owner_id => "1",
+      :owner_type => "value for owner_type"
     }
   end
 
